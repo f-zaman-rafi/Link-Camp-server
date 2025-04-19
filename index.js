4ba5af98020b0a0ceb49dfec7bbc7eef7ee380ae
f-zaman-rafi <f.zaman.rafi@gmail.com>
2025-04-19 23:45:59 +0800

    building server for linkcamp

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+// const uri = `mongodb+srv://linkcamp:kLRyvOZY8DgeW1Iq@cluster0.okia5sv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.okia5sv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASS:", process.env.DB_PASS);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Simple crud is running");
+});
+
+app.listen(port, () => {
+  console.log(`simple crud is running on port; ${port}`);
+});
+
+// linkcamp
+
+// kLRyvOZY8DgeW1Iq
